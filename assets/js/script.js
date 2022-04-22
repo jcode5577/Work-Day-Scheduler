ed0a269904db248ecc4171498145aef6216cf52b
jcode5577 <99107295+jcode5577@users.noreply.github.com>
2022-04-22 15:34:09 -0500

    final project touch up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
+//displays current date and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+// uses a for loop to prevent using repeating code for localStorage    
 for (let i = 9; i < 18; i++) {
-  //var timeBlockEl =document.querySelector("#hour9")
+  
   var timeBlockEl = $("#hour" + i);
   var buttonEl = timeBlockEl.children("button");
   console.log(buttonEl);
-  //buttonEl.addEventListener("click", saveText)
+  
   buttonEl.on("click", function () {
     var textArea = $("#text" + i);
     console.log(textArea.val());
     var text = textArea.val();
     localStorage.setItem("data-" + i, text);
-  });
+    });
 
-  // for loading
+  // for loading local storage 
   var textArea = $("#text" + i);
   var text = localStorage.getItem("data-" + i);
   console.log(text);
   textArea.val(text);
-}
+    }
